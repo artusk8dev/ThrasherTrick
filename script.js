2420cd81bcb9a42f5b5f1b6022cd80e72ec0c0f5
Arthur Selingin <arthur_santos@outlook.com.br>
2024-07-09 15:45:32 -0300

    deixando o carrossel mais bonito

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const carrossel = document.getElementById('img')
+document.addEventListener("DOMContentLoaded", function () {
+  const carrossel = document.getElementById("imgContainer");
+  const itensCarrossel = document.getElementsByClassName("item-Carrossel");
+  let contador = 0;
 
-    function moveCarrossel(){
-        const primeiroItem = carrossel.firstElementChild;
-        
-        carrossel.appendChild(primeiroItem);
-        carrossel.style.transform = 'translateX(0)';
-        carrossel.style.transition = '0';
-
-        setTimeout(()=> {
-            carrossel.style.transition = 'transform 0.5s ease-in-out';
-        });
+  function moveCarrossel() {
+    contador++;
+    var primeiroItem = carrossel.children[contador];
+    console.log(primeiroItem);
+    var itemWidth = carrossel.children[contador].clientWidth;
+    console.log(itemWidth);
+    if ((carrossel.children[contador].clientWidth = 4)) {
+      itemWidth = 400;
     }
-    setInterval(moveCarrossel, 2500);
-})
+    // Move carousel to show next item
+    carrossel.style.transition = "transform 2s ease";
+    carrossel.style.transform = `translateX(-${contador * itemWidth}px`;
 
+    if (contador >= 4) {
+      contador = 0;
+      carrossel.style.transform = "translateX(0)";
+    }
+  }
+
+  setInterval(moveCarrossel, 2500);
+});
